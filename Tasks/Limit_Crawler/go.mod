fca9d2b5646c1945fec6ab5644d7b9ee332ff285
Chris-Mwiti <chrismwiti21@gmail.com>
2026-08-06 09:29:16 +0300

    changes: Reinitialization of the repository to suit the structure wanted

---
+++ Tasks/Limit_Crawler/go.mod
@@ -0,0 +1,3 @@
+module github.com/Chris-Mwiti/Go_Concurrency.git
+
+go 1.26.1
---
+++ Theory/sync_cond.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var done bool
+
+func read(c *sync.Cond, name string){
+	fmt.Println("reading process has began")
+	//here we are locking since we have a critical section which is done var
+	c.L.Lock()
+	for !done {
+		c.Wait()
+	}
+	time.Sleep(2 * time.Second)
+	fmt.Printf("%s has started reading", name)
+	c.L.Unlock()
+}
+
+func write(c *sync.Cond, name string){
+	fmt.Printf("writing process has began: %s", name)
+
+	c.L.Lock()
+	time.Sleep(3 * time.Second)
+	done = true
+	c.L.Unlock()
+
+	fmt.Println("broadcasting the event to sleeping goroutines")
+	c.Broadcast()
+}
+
+func main() {
+	var m sync.Mutex
+
+	cond := sync.NewCond(&m)
+
+	go read(cond, "Reader One")
+	go read(cond, "Reader Two")
+	go read(cond, "Reader Three")
+
+	write(cond, "Writer One")
+
+	time.Sleep(7 * time.Second)
+}
