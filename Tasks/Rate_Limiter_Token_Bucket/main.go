830bbc92087ff66b9d128de1596cb7cffbac9929
Chris-Mwiti <chrismwiti21@gmail.com>
2026-08-07 11:54:05 +0300

    changes: Implemented changes to the architecture of the distributed_rate_limiter: 1. There were deadlock issues within how I was checking for refilling details 2. Removed passing down the sync.Cond variable into functions instead opted to encapsulate them within a struct

--- Tasks/Rate_Limiter_Token_Bucket/main.go
+++ Tasks/Rate_Limiter_Token_Bucket/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
@@ -11,64 +13,110 @@ import (
 type TokenBucket struct {
 	maxCapacity int
 	rem int
-	refiling bool
-	ticker time.Ticker
+	mu sync.Mutex
+	cond *sync.Cond
 }
 
 type Node struct {
-	tokenBucket TokenBucket
+	tokenBucket *TokenBucket
+	client *http.Client
+	logger *slog.Logger
 }
 
-func (n *Node) DoReq(ctx context.Context, url string, cond *sync.Cond) (error) {
-	select {
-	case <-ctx.Done():
-		return fmt.Errorf("context timeout")
-	default:
-		cond.L.Lock()
-		for n.tokenBucket.rem <= 0 || n.tokenBucket.refiling{
-			cond.Wait()
-		}
-		n.tokenBucket.rem--
-		cond.L.Unlock()
-
-		time.Sleep(3 * time.Second)
-
-		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-
-		if err != nil {
-			return err
-		}
-
-		_, err = http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		} 
-
-		return nil
-
+func NewTokenBucket(maxcapacity int) TokenBucket{
+	tb := TokenBucket{
+		maxCapacity: maxcapacity,
+		rem: maxcapacity,
 	}
-
+	tb.cond = sync.NewCond(&tb.mu)
+	
+	return tb
 }
 
-func (n *Node) RefilBuck(ctx context.Context, cond *sync.Cond)(error) {
+func (n *Node) acquireToken(ctx context.Context) (error) {
 
-	select {
-	case <-ctx.Done():
-		return fmt.Errorf("context done")
+	n.tokenBucket.mu.Lock()
+	defer n.tokenBucket.mu.Unlock()
 
-	case <-n.tokenBucket.ticker.C:
-		cond.L.Lock()
-		defer cond.L.Unlock()
-		if n.tokenBucket.rem < n.tokenBucket.maxCapacity {
-			n.tokenBucket.refiling = true
-			n.tokenBucket.rem += 2
-			cond.Broadcast()
-		} else {
-			return fmt.Errorf("token bucket already full")
+	//create an after ctx func that will trigger a wake up to all other goroutines to check for ctx.Err
+	stopCtx := context.AfterFunc(ctx, func() {
+		n.tokenBucket.mu.Lock()
+		n.tokenBucket.cond.Broadcast()
+		n.tokenBucket.mu.Unlock()
+	})
+	defer stopCtx()
+
+	for n.tokenBucket.rem <= 0 {
+		if err := ctx.Err(); err != nil {
+			return fmt.Errorf("context already done and completed")
 		}
+		n.tokenBucket.cond.Wait()
 	}
+
+	//here we are going to add another check to check if the the context is already complete before deducting the token
+	if err := ctx.Err(); err != nil {
+		return fmt.Errorf("context already done. canceled or timedout")
+	}
+
+	//deduct the token buck rem tokens
+	n.tokenBucket.rem--
 
 	return nil
+}
+
+func (n *Node) DoReq(ctx context.Context, url string) (error) {
+	if err := n.acquireToken(ctx); err != nil {
+		return fmt.Errorf("req cannot acquire token")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+
+		slog.ErrorContext(ctx, "error while formatting request", "err", err)
+		return fmt.Errorf("error while making request")
+	}
+
+	resp, err := n.client.Do(req)
+	if err != nil{
+		slog.ErrorContext(ctx, "error while submitting request", "err", err)
+		return fmt.Errorf("error while submitting request")
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func (n *Node) RefilBuck(ctx context.Context, interval time.Duration, tokenRefil int)(error) {
+	//create an method ticker for the refil bucket
+	//this is crucial since a shared timer can affect how the signal is submitted 
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context already done")
+
+		case <-ticker.C:
+			n.tokenBucket.mu.Lock()
+			if n.tokenBucket.rem < n.tokenBucket.maxCapacity {
+				n.tokenBucket.rem = min(n.tokenBucket.maxCapacity, (n.tokenBucket.rem + tokenRefil))
+				//unblock other goroutines awaiting the response
+				n.tokenBucket.cond.Broadcast()
+			}
+			n.tokenBucket.mu.Unlock()
+		}	
+	}
+}
+
+func NewNode(bucket *TokenBucket, maxCapacity int, client *http.Client) Node {
+	buck := NewTokenBucket(maxCapacity)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+	return Node{
+		tokenBucket: &buck,
+		client: http.DefaultClient,
+		logger: logger,
+	}
 }
 
 func main() {
