d0abdc71e8e7ecdb13280a215c3e2c75aeb59e7c
Chris-Mwiti <chrismwiti21@gmail.com>
2026-08-09 13:32:25 +0300

    changes: Okay am trying to implement a group state commit storage engine: so far implement the tcp connection, trying to figure out how to handle the engine stage

---
+++ Tasks/Group_Commit_Storage_Engine/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"net"
+	"sync"
+)
+
+type GroupCommitNode struct {
+	Name string
+	buffer *bytes.Buffer
+	mu sync.Mutex
+	cond *sync.Cond
+
+	//network
+	proto string
+	addr string
+	l net.Listener
+
+
+	//suppl
+	logger *slog.Logger
+}
+
+type Client struct {
+	conn net.Conn
+	logger *slog.Logger
+	cond *sync.Cond
+}
+
+func (c *Client) HandleConn(ctx context.Context, sendCh chan <-[]byte) (error) {
+
+	//defer client connection closing
+	defer c.conn.Close()
+	c.cond.L.Lock()
+	defer c.cond.L.Unlock()
+	buf := new(bytes.Buffer)
+	for {
+		if err := ctx.Err(); err != nil {
+			c.logger.ErrorContext(ctx, "context done", "err", err)
+			return err
+		}
+		//@todo: I dont know if this is okay to do...
+		buf.Reset()
+		_, err := c.conn.Read(buf.AvailableBuffer())
+		if err != nil {
+			c.logger.ErrorContext(ctx, "error while reading conn", "err", err.Error())
+			return err
+		}
+
+		//send the data over the channel
+		sendCh <- buf.Bytes()
+
+		//block the connection until to receive a signal to resume
+		c.cond.Wait()
+
+		//so for here I don't know what will be the terminater so that I know its an EOF
+		//that will lead to a break out of loop scenario
+	}
+}
+
+
+func NewNode(name string, proto, addr string, logger *slog.Logger) (*GroupCommitNode, error){
+	node := GroupCommitNode{
+		Name: name,
+		buffer: new(bytes.Buffer),	
+	}
+	node.cond = sync.NewCond(&node.mu)
+	node.logger = logger
+	
+	//establish the network ports
+	node.proto = proto
+	node.addr = addr
+	l, err := net.Listen(proto, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	node.l = l
+
+	return &node, nil
+} 
+
+func (n *GroupCommitNode) Listen(ctx context.Context) (error) {
+	//spawn a new go routine to handle connections -> but this time create a client and attach a conn to handle those connections
+	for {
+
+		if err := ctx.Err(); err != nil {
+			n.logger.ErrorContext(ctx, "context done", "err", err.Error())
+			return err
+		}
+
+		conn, err := n.l.Accept()
+		if err != nil {
+			//log the error but continue to accept incoming connectoins
+			n.logger.WarnContext(ctx, "warning, error while accepting connection", "err", err.Error())
+			continue
+		}
+
+		client := Client{conn: conn}
+
+		//establish a goroutine to handle client connections
+		go func (ctx context.Context, client Client){
+			//create a client handler
+
+		}(ctx, client)
+	}
+}
